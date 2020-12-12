6bfaa1c97bfec2b5cf35ca2f48bab6cb04bdb582
Lansana <lansana.diomande@epfedu.fr>
2020-12-12 20:54:29 +0100

    Begin of API
    I use data of aviation weather center and i convert them into associative array

---
+++ controller.php
@@ -0,0 +1,28 @@
+<?php
+// Here my fonction
+
+function get_xml_of_aviation_weather_center(){
+    $lines = file_get_contents('https://www.aviationweather.gov/adds/dataserver_current/current/metars.cache.xml');
+    $xml = simplexml_load_string($lines);
+    return $xml;
+}
+
+function convert_xml_to_array_associative($xml_data){
+    $json_data = json_encode($xml_data);
+    $array_associative = json_decode($json_data,true);
+    return $array_associative;
+}
+
+function get_weather_data(){
+    
+    $xml_data = get_xml_of_aviation_weather_center();
+    $array_associative = convert_xml_to_array_associative($xml_data);
+
+    return $array_associative;
+}
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// The router
+
+require_once('controller.php');
+
+if(isset($_GET['data'])){
+
+ echo $_GET['data'];
+
+}else{
+
+  
+  
+  //print_r($array_associative['data']['METAR'][0]) ;
+  $array_associative = get_weather_data();
+  print_r($array_associative);
+
+  /*foreach($array_associative['data']['METAR'] as $data){
+    if($data['station_id'] == 'LFMN'){
+      echo $data['raw_text'];
+    }
+    
+  }*/
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
